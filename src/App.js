e1cffcc0b0efcde851d62aee80500eb3f392e8f8
MaRtin322s <marti.sofroniev0408@gmail.com>
2022-12-15 17:42:11 +0200

    add created task

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import "./App.css";
+import React, {useState, useEffect}  from "react";
+import Document from "./Document";
 
 function App() {
+  const [content, setContent]=useState('');
+
+  useEffect(() => {
+    fetch('https://jaspervdj.be/lorem-markdownum/markdown.txt')
+    .then((response) => response.text())
+    .then((response) => {setContent(response);});
+  }, []);
+
+  const title = 'Terms and Conditions';
+
   return (
     <div className="App">
-      <section class="hero">
-        <div class="hero-body">
-          <p class="title">A React Task</p>
-          <p class="subtitle">by Boom.dev</p>
+      <section className="hero">
+        <div className="hero-body">
+          <p className="title">A React Task</p>
+          <p className="subtitle">by Boom.dev</p>
         </div>
       </section>
-      <div class="container is-fullhd">
-        <div class="notification">
-          Edit the <code>./src</code> folder to add components.
+      <div className="container is-fullhd">
+        <div className="notification">
+          <Document content={content} title={title} />
         </div>
       </div>
     </div>
